b747a4434eb6b556bedd98e8caf51a3e3fd7b797
artemtema <temaboychuk@gmail.com>
2023-01-30 13:11:13 +0530

    сделал Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] arr = new int[8];
+
+Console.WriteLine("Введите 8 (Восемь) целых чисел для создания массива");
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Вы создали массив:");
+Console.Write(string.Join(", ", arr));
